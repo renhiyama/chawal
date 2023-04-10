db3a62b4e007dfc4470c183cd1ff4786b174258e
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-10 17:53:54 +0000

    test

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+export default function(){
+	return(
+		<div className="bg-yellow-200 min:h-screen">
+		{ /* vertical centering */ }
+		<div className="flex flex-col justify-center h-full">
+		<h1 className="text-yellow-600 text-4xl text-center">Welcome Chawal!</h1>
+		<p className="text-yellow-700 text-center">Yes you - chawal. This website is for you. Enjoy 🍚</p>
+		</div>
+		</div>
+	)
+		}
