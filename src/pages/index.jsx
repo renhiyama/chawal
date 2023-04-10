e2b28fcb18298067cebbab47d9e61deb27fa5da7
Ren Hiyama <renhiyama@rovelstars.com>
2023-04-10 18:21:30 +0000

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,3 @@
-import {useState} from 'react';
-import Island from '@reejs/react/island';
 export default function(){
 	return(
 		<div className="bg-yellow-200 min-h-screen">
@@ -7,18 +5,15 @@ export default function(){
 		<div className="flex flex-col justify-center h-full">
 		<h1 className="text-yellow-600 text-4xl font-bold text-center">Welcome Chawal!</h1>
 		<p className="text-yellow-700 font-semibold text-center">Yes you - chawal. This website is for you. Enjoy!</p>
-		<Island on="user:click">
 		<RiceButton />
-		</Island> 
 		</div>
 		</div>
 	)
 		}
 
 export function RiceButton(){
-	const [rice, setRice] = useState(0);
 	return(
-		<button onClick={() => setRice(rice + 1)}
-		className="bg-yellow-600 text-yellow-200 font-semibold px-4 py-2 rounded-lg">Total Rice Eaten 🍚 {rice}</button>
+		<a href="https://youtu.be/vEz2X8fXizw" target="_blank" rel="noreferrer"
+		className="bg-yellow-600 text-yellow-200 font-semibold px-4 py-2 rounded-lg mx-auto text-center">How to make Chawal</a>
 	)
 }
